35d7aeaf7c77745c517ba576dcce800d0944f9b7
Daniel <daniel@Daniels-iMac.local>
2019-06-05 08:43:06 -0700

    Initial Commit

---
+++ PrograDiccionario/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PrograDiccionario
+//
+//  Created by Daniel on 05/06/2019.
+//  Copyright © 2019 Daniel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
