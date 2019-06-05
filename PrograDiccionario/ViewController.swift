4c687dd31bccb998c2a905d63adc3c33b0248c1e
Daniel <daniel@Daniels-iMac.local>
2019-06-05 09:17:38 -0700

    Terminado

--- PrograDiccionario/ViewController.swift
+++ PrograDiccionario/ViewController.swift
@@ -7,14 +7,40 @@
 //
 
 import UIKit
+import WebKit
+class ViewController: UIViewController, UIWebViewDelegate {
 
-class ViewController: UIViewController {
-
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var searchButton: UIButton!
+    @IBOutlet weak var webView: WKWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
 
+    @IBAction func buscarPalabra(_ sender: Any) {
+        var pal = "\(textField.text!)"
+        var buscarPal = pal.replacingOccurrences(of: " ", with: "_")
+        print("Se vaa buscar\(buscarPal)" )
+        
+        var palCompleta: String = "https://es.wikipedia.org/wiki/\(buscarPal)"
+        if  let urlBuscar = URL(string: "\(palCompleta)"){
+            print("Se vaa buscar\(urlBuscar)" )
+            let request = URLRequest(url: urlBuscar)
+            print("url es; \(request.url?.absoluteString)")
+            if (request.url?.absoluteString == palCompleta){
+                
+                print("url resultante: \(request.url?.absoluteString)")
+                webView.load(request as URLRequest)
+                textField.text = ""
+            }else{
+                textField.text = "El texto introducido no se ha encontrado en wikipedia"
+            }
+        }else{
+            textField.text = "El texto introducido no se ha encontrado en wikipedia"
+        }
+    }
 }
 
